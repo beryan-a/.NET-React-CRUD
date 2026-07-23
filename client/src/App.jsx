257dc2225e2dcb1354d2d24e2aae50fc9132d70c
Beryan Albeik <beryanalbeik@gmail.com>
2026-07-23 11:55:28 +0300

    Active Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar';
+import { Routes, Route } from "react-router";
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { NotFound } from './pages/NotFound';
+import { Person } from './components/person/Person';
 import './App.css'
 
 function App() {
-
+  
   return (
     <>
+      {/* Navigation */}
       <Navbar/>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/Person" element={<Person/>}/>
+        <Route path="" element={<NotFound/>}/>
+      </Routes>
+
     </>
     
   )
