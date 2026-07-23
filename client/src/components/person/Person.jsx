0af8ec9691ab6193ba45e50f359d5cf5b065b634
Beryan Albeik <beryanalbeik@gmail.com>
2026-07-23 13:55:39 +0300

    bitsin artık

--- client/src/components/person/Person.jsx
+++ client/src/components/person/Person.jsx
@@ -2,6 +2,14 @@ import PersonForm from "./PersonForm"
 import PersonList from "./PersonList"
 
 function Person() {
+
+  const _people = [
+    {id:1, firstName: 'John', lastName: 'Doe'},
+    {id:2, firstName: 'Micheal', lastName: 'Scott'},
+    {id:3, firstName: 'Pam', lastName: 'Beesly'},
+    {id:4, firstName: 'Ryan', lastName: 'Howard'},
+  ]
+
     return (
         <div className="min-h-screen bg-gray-50 py-8">
             <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 space-y-6">
@@ -13,7 +21,7 @@ function Person() {
                 </div>
 
                 <PersonForm />
-                <PersonList />
+                <PersonList people= {_people}/>
             </div>
         </div>
     )
